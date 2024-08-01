9a11b1a01a7f2f8b4d167e46249b9ed70a0c8304
prabhat pankaj <prabhatiitbhu@gmail.com>
2024-08-01 06:55:13 +0530

    initial

---
+++ orbit-lib/src/main/java/com/cars24/orbit_lib/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.cars24.orbit_lib;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printlog(String tag, String message) {
+        Log.d(tag, message);
+    }
+}
